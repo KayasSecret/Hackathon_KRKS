996cb1c3c49779491fe7c9ee808abee2b08215ce
Guapt <158696969+Guapt@users.noreply.github.com>
2024-02-08 20:13:41 +0530

    Update intex.php

--- intex.php
+++ intex.php
@@ -12,21 +12,12 @@
     }
     echo "Success fully connected to database";
     
-    $sql = "CREATE DATABASE IF NOT EXISTS myDB";
+    
     if ($conn->query($sql) === TRUE) {
         echo "Database created successfully";
     } else {
         echo "Error creating database: " . $conn->error;
     }
-    
-    $sql = "CREATE TABLE IF NOT EXISTS MyGuests (
-                                    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-                                    username VARCHAR(30) NOT NULL,
-                                    email VARCHAR(50),
-                                    reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-    )";
-    
-    
 
     if ($conn->query($sql) === TRUE) {
         echo "Table MyGuests created successfully";
