9f6ae9383aa7533fb1811858c23a533ea99391ad
gsneha16 <gsneha2319@gmail.com>
2024-02-09 17:57:32 +0530

    new

--- card.js
+++ card.js
@@ -11,13 +11,49 @@ let newBtn = document.querySelector(".new-btn");
 
 let cards = document.querySelector(".wrapper");
 
+// const details =[
+//     {
+//         image :'',
+//         title :'sneha Goyal',
+//         price: 230,
+//         add : 'maihr',
+//     }
+// ]
+
+// const categ = [...new Set (details.map((item)=>{
+//     return item
+// }))]
+
+// let i=0;
+// document.getElementsByClassName(".card-s").innerHTML =details.map((item)=>{
+//    var{image , title ,price} = item;
+//    return(
+//     `<img src="${image} alt="">
+//     <h1>${Name.value}</h1>
+//     <p>Rent House Price : ${Price.value}</p>
+//     <p> <i class="fa-solid fa-location-dot"></i>${Address.value}</p>
+//     <a href="/Cards-info/card.html" class="btn">Read More</a>
+//     `
+//    )
+// }).join('')
+
+
+
 const addCard = ()=> {
     let newCard = document.createElement('div');
     newCard.classList.add("card-s"); 
     cards.append(newCard);
 
     let addressVal = Address.value;
-    newCard.innerHTML 
+    let newdiv = document.createElement('div');
+    newdiv.classList.add("info"); 
+    newdiv.innerHTML = `
+    <h1>${Name.value}</h1>
+    <p>Rent House Price : ${Price.value}</p>
+    <p> <i class="fa-solid fa-location-dot"></i>${Address.value}</p>
+    <a href="/Cards-info/card.html" class="btn">Read More</a>
+    `
+    cards.append(newdiv);
 }
 
 
