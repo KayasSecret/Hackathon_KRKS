a50220880ea966c853762483f40dd1a1e8c0d797
gsneha16 <gsneha2319@gmail.com>
2024-02-09 22:27:41 +0530

    card info

--- card.js
+++ card.js
@@ -5,6 +5,8 @@ let Pic = document.querySelector("#file");
 let Room = document.querySelector("#room");
 let cards = document.querySelector(".wrapper");
 
+// creating card
+
 const addCard = ()=> {
     let newCard = document.createElement('div');
     newCard.classList.add("card-s"); 
@@ -17,13 +19,36 @@ const addCard = ()=> {
         <p>Rent House Price : ${Price.value}</p>
         <p> <i class="fa-solid fa-location-dot"></i> ${Address.value}</p>
         <p>Room : ${Room.value}</P>
-        <a href="/Cards-info/card.html" class="btn">Read More</a>
+        <a onclick = "createCardInfo();  event.preventDefault();" class="btn">Read More</a>
     </div>`
-    newCard.innerHTML = data;
 }
 
-// let Email = document.querySelector("#email");
-// let Conatct = document.querySelector("#contact");
-// let Floor = document.querySelector("#floor");
-// let Reg_daete = document.querySelector("#reg_date");
-// let newBtn = document.querySelector(".new-btn");
+let Email = document.querySelector("#email");
+let Conatct = document.querySelector("#contact");
+let Floor = document.querySelector("#floor");
+let Reg_daete = document.querySelector("#reg_date");
+let newBtn = document.querySelector(".new-btn");
+
+// creating Card-info
+
+const createCardInfo = () =>{
+    let newCardInfo = document.createElement("div");
+    newCardInfo.classList.add("card_info");
+    document.getElementsByTagName("body").append(newCardInfo);
+
+    let newData = newCardInfo.innerHTML= `
+    <div class="more_img">
+            <div  class="m_img" style="background-image: url(/images/House-image/k1.jpg);"></div>
+            <div class="m_img" style="background-image: url(/images/House-image/k2.jpg);"></div>
+            <div class="m_img" style="background-image: url(/images/House-image/k1.jpg);"></div>
+            <div class="m_img" style="background-image: url(/images/House-image/k3.jpg);"></div>
+        </div>     
+        <div class="more_info">
+            <p> <i class="fa-solid fa-money-bill"></i> Price: ${Price.value}rs / month</p>
+            <p> <i class="fa-regular fa-square"></i> Rooms : ${Room.value}</p> 
+            <p> <i class="fa-solid fa-stairs"></i> Floor : ${Floor.value}</p>
+
+            <p> <i class="fa-solid fa-tag"></i> Registered on : ${Reg_daete} </p>
+        </div>
+    `
+}
