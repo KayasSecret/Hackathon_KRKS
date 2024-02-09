cba73096d8ae82a272eff33ad6ef234b94da44aa
gsneha16 <gsneha2319@gmail.com>
2024-02-09 15:41:29 +0530

    adding card automatically

---
+++ card.js
@@ -0,0 +1,25 @@
+let Name = document.querySelector("#name");
+let Email = document.querySelector("#email");
+let Conatct = document.querySelector("#contact");
+let Address = document.querySelector("#address");
+let Room = document.querySelector("#room");
+let Price = document.querySelector("#price");
+let Floor = document.querySelector("#floor");
+let Reg_daete = document.querySelector("#reg_date");
+let Pic = document.querySelector("#file");
+let newBtn = document.querySelector(".new-btn");
+
+let cards = document.querySelector(".wrapper");
+
+// newBtn.addEventListener("click", (evt)=>{
+//     evt.preventDefault();
+// });
+
+const addCard = ()=> {
+    let newCard = document.createElement('div');
+    newCard.classList.add("card-s"); 
+    cards.append(newCard);
+    console.log("hello");
+}
+
+
