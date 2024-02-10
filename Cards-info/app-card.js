c28abb5ba5a7d45ca7b60bdb6bb2d9394525b721
gsneha16 <gsneha2319@gmail.com>
2024-02-10 19:59:30 +0530

    final , now you can add review

--- Cards-info/app-card.js
+++ Cards-info/app-card.js
@@ -1,21 +1,39 @@
-var currentDateTime = new Date();
-var year = currentDateTime.getFullYear();
-var month = (currentDateTime.getMonth() + 1);
-var date = (currentDateTime.getDate() + 1);
+// ======By kayas
 
-if(date < 10) {
-  date = '0' + date;
-}
-if(month < 10) {
-  month = '0' + month;
-}
+// var currentDateTime = new Date();
+// var year = currentDateTime.getFullYear();
+// var month = (currentDateTime.getMonth() + 1);
+// var date = (currentDateTime.getDate() + 1);
 
-var dateTomorrow = year + "-" + month + "-" + date;
-var checkinElem = document.querySelector("#checkin-date");
-var checkoutElem = document.querySelector("#checkout-date");
+// if(date < 10) {
+//   date = '0' + date;
+// }
+// if(month < 10) {
+//   month = '0' + month;
+// }
 
-checkinElem.setAttribute("min", dateTomorrow);
+// var dateTomorrow = year + "-" + month + "-" + date;
+// var checkinElem = document.querySelector("#checkin-date");
+// var checkoutElem = document.querySelector("#checkout-date");
 
-checkinElem.onchange = function () {
-    checkoutElem.setAttribute("min", this.value);
-}
+// checkinElem.setAttribute("min", dateTomorrow);
+
+// checkinElem.onchange = function () {
+//     checkoutElem.setAttribute("min", this.value);
+// }
+
+
+// ===By Sneha 
+// Review Add js
+
+let custName = document.querySelector(".custName");
+let revDate = document.querySelector(".revDate");
+let rev = document.querySelector(".rev");
+let revAddBtn = document.querySelector(".revAdd");
+let revList = document.querySelector(".revList");
+
+revAddBtn.addEventListener("click", ()=>{
+    let newRev = document.createElement('li');
+    let revData = newRev.innerText = `${revDate} ${rev.value} ~ ${custName.value}`;
+    revList.append(revData);
+})
